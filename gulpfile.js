e0c038cea89d22f16c42f29c9bf283afb4016340
StanislavChunikhin <s.tchunihin@ya.ru>
2018-08-27 12:32:41 +0300

    Installing Gulp Dependencies.

---
+++ gulpfile.js
@@ -0,0 +1,117 @@
+'use strict';
+
+var gulp = require('gulp'),
+  watch = require('gulp-watch'),
+  prefixer = require('gulp-autoprefixer'),
+  uglify = require('gulp-uglify'),
+  less = require('gulp-less'),
+  sourcemaps = require('gulp-sourcemaps'),
+  rigger = require('gulp-rigger'),
+  cssmin = require('gulp-clean-css'),
+  imagemin = require('gulp-imagemin'),
+  pngquant = require('imagemin-pngquant'),
+  rimraf = require('rimraf'),
+  browserSync = require("browser-sync"),
+  reload = browserSync.reload;
+
+
+var path = {
+  build: {
+      html: 'build/',
+      js: 'build/js/',
+      css: 'build/css/',
+      img: 'build/img/',
+      fonts: 'build/fonts/'
+  },
+  src: { 
+      html: 'app/*.html', 
+      js: 'app/js/*.js',
+      style: 'app/less/main.less',
+      img: 'app/img/**/*.*',
+      fonts: 'app/fonts/**/*.*'
+  },
+  watch: { 
+      html: 'app/**/*.html',
+      js: 'app/js/**/*.js',
+      style: 'app/less/**/*.less',
+      img: 'app/img/**/*.*',
+      fonts: 'app/fonts/**/*.*'
+  },
+  clean: './build'
+};
+
+var config = {
+  server: {
+      baseDir: "./build"
+  },
+  tunnel: true,
+  host: 'localhost',
+  port: 9000,
+  logPrefix: "Stanislav_Chunikhin"
+};
+
+gulp.task('html:build', function () {
+  gulp.src(path.src.html) 
+      .pipe(rigger()) 
+      .pipe(gulp.dest(path.build.html)) 
+      .pipe(reload({stream: true})); 
+});
+
+
+gulp.task('style:build', function () {
+  gulp.src(path.src.style) 
+      .pipe(sourcemaps.init()) 
+      .pipe(less()) 
+      .pipe(prefixer()) 
+    //.pipe(cssmin()) 
+      .pipe(gulp.dest(path.build.css)) 
+      .pipe(reload({stream: true}));
+});
+
+gulp.task('img:build', function () {
+  gulp.src(path.src.img) 
+      .pipe(gulp.dest(path.build.img)) 
+      .pipe(reload({stream: true})); 
+});
+
+gulp.task('js:build', function () {
+  gulp.src(path.src.js) 
+      .pipe(gulp.dest(path.build.js)) 
+      .pipe(reload({stream: true})); 
+});
+
+
+
+gulp.task('build', [
+  'html:build',
+  'style:build',
+  'img:build',
+  'js:build'
+]);
+
+gulp.task('watch', function(){
+  watch([path.watch.html], function(event, cb) {
+      gulp.start('html:build');
+  });
+  watch([path.watch.style], function(event, cb) {
+      gulp.start('style:build');
+  });
+  watch([path.watch.img], function(event, cb) {
+      gulp.start('img:build');
+  });
+  watch([path.watch.js], function(event, cb) {
+      gulp.start('js:build');
+  });
+  
+});
+
+
+gulp.task('webserver', function () {
+  browserSync(config);
+});
+
+gulp.task('clean', function (cb) {
+  rimraf(path.clean, cb);
+});
+
+gulp.task('default', ['build', 'webserver', 'watch']);
